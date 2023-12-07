b4e448fd5cccd9ac08f05886ec4d80c14d956d18
henrotta <alexandre.henrotte@student.unamur.be>
2023-12-07 09:04:27 +0100

    feat(back-end): setup guest_book endpoint

--- back-end/src/app.py
+++ back-end/src/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def guest_book():
+
+    if request.method == "GET":
+        limit = request.args.get("limit")
+        if limit:
+            return "<p>Hello, World!</p>" + limit
+        else:
+            return "<p>Hello, World!</p>"
+
+    elif request.method == "POST":
+        message = request.json["message"]
+        return 'Message saved : "' + message + '"'
+
+
