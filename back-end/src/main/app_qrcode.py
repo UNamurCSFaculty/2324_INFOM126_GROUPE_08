c78ad18db13cef00de3b3d7c9df9ea8be8076460
henrotta <alexandre.henrotte@student.unamur.be>
2023-12-07 09:31:34 +0100

    feat(back-end): handle qrcode's url in the body of the endpoint

--- back-end/src/main/app_qrcode.py
+++ back-end/src/main/app_qrcode.py
@@ -1,17 +1,19 @@
-from flask import Blueprint, send_file
+from flask import Blueprint, request, send_file
 import qrcode
 
 app_qrcode = Blueprint('qrcode',__name__)
 
 @app_qrcode.route("/qrcode", methods=["POST"])
 def qr_code():
+    url = request.json["url"]
+
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data('https://example.com')
+    qr.add_data(url)
     qr.make(fit=True)
 
     img = qr.make_image(fill_color="black", back_color="white")
