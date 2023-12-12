079ef1be487c36370b01d8c3d947f04fb50885e4
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-12 16:54:43 +0100

    feat(database): add SQLAlchemy models to backend

--- back-end/src/main/models.py
+++ back-end/src/main/models.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, DateTime, String
+from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import DeclarativeBase
 from flask_sqlalchemy import SQLAlchemy
 
 
-# db config
+#
 class Base(DeclarativeBase):
     pass
 
@@ -12,8 +12,13 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 
+class Message(db.Model):
+    id_msg = mapped_column(Integer, primary_key=True)
+    date = mapped_column(DateTime)
+    text = mapped_column(String(500))
 
-# class Message(db.Model):
-#     id_msg = Column(Integer, primary_key=True)
-#     date: Mapped[Date]
+
+class Domain(db.Model):
+    name = mapped_column(String(255), primary_key=True)
+    count = mapped_column(Integer)
 
