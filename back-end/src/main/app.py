c3812c98c0dadc4804aff91d14a2d4aad691faa3
Louis Cavrenne <124626955+louisc-un@users.noreply.github.com>
2023-12-12 17:21:52 +0100

    Feature 12 : Create Base Class (models) for SQLAlchemy (#39)
    * refactor(back-end): create new files and refactor project before creating models
    
    * refactor(back-end): move file content in new created files
    
    * refactor(back-end): delete useless files
    
    * refactor(back-end): move params to configs file
    
    * feat(database): add SQLAlchemy models to backend
    
    * feat(database): add Mapped type
    
    * feat(back-end): add class models
    
    * refactor(back-end): remove useless import

--- back-end/src/main/app.py
+++ back-end/src/main/app.py
@@ -1,31 +1,26 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 
-from app_qrcode import app_qrcode
-from app_guest_book import app_guest_book
-
-
-# db config
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
+from endpoints import guest_book_blueprint, qrcode_blueprint
+from models import db
+from configs import Database, API
 
 
-
+# create Flask app
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = Database.URI
+
 # initialize the app with the extension
 db.init_app(app)
-# register blueprint
-app.register_blueprint(app_guest_book)
-app.register_blueprint(app_qrcode)
 
+# create tables if not created yet
+with app.app_context():
+    db.create_all()
 
-
+# register blueprints
+app.register_blueprint(guest_book_blueprint)
+app.register_blueprint(qrcode_blueprint)
 
 
 # launch
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=API.HOST, port=API.PORT)
