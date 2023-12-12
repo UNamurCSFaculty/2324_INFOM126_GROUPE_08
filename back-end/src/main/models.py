9ef32edff318d5cd0e47223d0b47f09e1726f2e2
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-12 16:56:13 +0100

    feat(database): add Mapped type

--- back-end/src/main/models.py
+++ back-end/src/main/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Integer, DateTime, String
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -13,12 +12,12 @@ db = SQLAlchemy(model_class=Base)
 
 
 class Message(db.Model):
-    id_msg = mapped_column(Integer, primary_key=True)
-    date = mapped_column(DateTime)
-    text = mapped_column(String(500))
+    id_msg: Mapped[Integer] = mapped_column(Integer, primary_key=True)
+    date: Mapped[DateTime] = mapped_column(DateTime)
+    text: Mapped[String] = mapped_column(String(500))
 
 
 class Domain(db.Model):
-    name = mapped_column(String(255), primary_key=True)
-    count = mapped_column(Integer)
+    name: Mapped[String] = mapped_column(String(255), primary_key=True)
+    count: Mapped[Integer] = mapped_column(Integer)
 
