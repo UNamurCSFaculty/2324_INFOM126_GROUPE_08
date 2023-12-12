5db220d1c19cc42603cf977fd77c4cb8e2e69fef
LeigerMax <47776833+LeigerMax@users.noreply.github.com>
2023-12-12 15:07:49 +0100

    feat(front-end) : add consolelog()

--- front-end/src/components/QRCodeGenerator.vue
+++ front-end/src/components/QRCodeGenerator.vue
@@ -1,22 +1,26 @@
-
 <script setup lang="ts">
 import { ref } from 'vue';
 import axios from 'axios';
 
 const url = ref('');
-const qrcode = ref('');
+const qrcodeUrl = ref('');
 
 const generateQRCode = async () => {
   try {
     console.log('Sending URL:', url.value);
+    
     const response = await axios.post('/qrcode', { url: url.value });
-    console.log('Response from server:', response.data);
-    qrcode.value = response.data;
+    
+    console.log(response);
+
+    qrcodeUrl.value = response.data;
+
   } catch (error) {
     console.error('Error generating QR code:', error);
   }
 };
 </script>
+
 
 <template>
   <div class="container mt-5">
@@ -28,7 +32,9 @@ const generateQRCode = async () => {
       </div>
       <button type="submit" class="btn btn-primary">Generate QR Code</button>
     </form>
-    <div id="qrcode" class="mt-4"></div>
+    <div id="qrcode" class="mt-4">
+      <img :src="qrcodeUrl" alt="Generated QR Code" v-if="qrcodeUrl">
+    </div>
   </div>
 </template>
 
