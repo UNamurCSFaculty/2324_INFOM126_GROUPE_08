18390c07be7b82aada929b7f1dad2cb97555bdf1
henrotta <113603832+henrotta@users.noreply.github.com>
2023-12-12 15:41:05 +0100

    Feature 36 : Add TypeScript ESLint stage in pipeline (#38)
    * feat(front-end): add eslinter and set a job in pipeline
    
    * feat(front-end): update eslint config
    
    * fix(project): fix missing pipeline step

---
+++ front-end/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    "root": true,
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser",
+    },
+    "extends": [
+        "plugin:vue/recommended",
+        "eslint:recommended"
+    ],
+    "plugins": ["@typescript-eslint"],
+    "rules": {
+        "vue/first-attribute-linebreak": ["error", {
+            "singleline": "ignore",
+            "multiline": "ignore"
+        }],
+        "vue/max-attributes-per-line": ["error", {
+            "singleline": {
+                "max": 7
+            },
+            "multiline": {
+                "max": 7
+            }
+        }],
+        "vue/html-closing-bracket-newline": 'off',
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/multiline-html-element-content-newline': 'off',
+        'vue/html-self-closing': 'off',
+        'vue/no-multiple-template-root': 'off'
+    }
+}
