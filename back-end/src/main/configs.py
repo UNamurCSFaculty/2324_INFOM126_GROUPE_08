5faddb27d7304958e24e6574f2a9a4fefe0e8317
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-12 17:42:12 +0100

    feat(database): set good settings for database connection

--- back-end/src/main/configs.py
+++ back-end/src/main/configs.py
@@ -1,5 +1,15 @@
 class Database:
-    URI = "sqlite:///project.db"
+    HOST = "0.0.0.0"
+    PORT = 5432
+    USERNAME = "postgres"
+    PASSWORD = "grespost"
+    DB = "INFOM126"
+
+    @classmethod
+    def URI(cls):
+        return "postgresql://{}:{}@{}:{}/{}".format(
+            cls.USERNAME, cls.PASSWORD, cls.HOST, cls.PORT, cls.DB
+        )
 
 
 class API:
