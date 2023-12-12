5039d038440fd9650f53a806a4b7cac5ea89569f
Louis Cavrenne <124626955+louisc-un@users.noreply.github.com>
2023-12-12 15:11:26 +0100

    feat(init): add SqlAlchemy to project (global commit) (#37)

--- back-end/src/main/app.py
+++ back-end/src/main/app.py
@@ -1,8 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
 from app_qrcode import app_qrcode
 from app_guest_book import app_guest_book
 
+
+# db config
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+# register blueprint
 app.register_blueprint(app_guest_book)
 app.register_blueprint(app_qrcode)
+
+
+
+
+
+# launch
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
