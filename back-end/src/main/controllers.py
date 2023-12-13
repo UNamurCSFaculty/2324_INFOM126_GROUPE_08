741cd95ff82d235dcb027b4c526b25992e679e90
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-13 19:58:13 +0100

    feat(back-end): implement guest_book_GET (WIP)

--- back-end/src/main/controllers.py
+++ back-end/src/main/controllers.py
@@ -1,12 +1,19 @@
+from flask import current_app
+
+from src.main.models import db, Message
 
 
 def guest_book_GET(limit=10):
+    with current_app.app_context():
+        stmt = db.select(Message).order_by(Message.date.desc())
+
+        messages = db.execute(stmt)
+
+        return [message.to_json() for message in messages]
+
+def guest_book_POST(session):
     pass
 
 
-def guest_book_POST():
-    pass
-
-
-def qu_code_POST():
+def qu_code_POST(session):
     pass
