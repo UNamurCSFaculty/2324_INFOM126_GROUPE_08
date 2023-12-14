9a63f01dc1e6eff08c9058166612919b794e913c
henrotta <alexandre.henrotte@student.unamur.be>
2023-12-14 17:02:20 +0100

    fix(front-end): fix qr code not showing in front-end

--- front-end/src/components/QRCodeGenerator.vue
+++ front-end/src/components/QRCodeGenerator.vue
@@ -3,18 +3,12 @@ import { ref } from 'vue';
 import axios from 'axios';
 
 const url = ref('');
-const qrcodeUrl = ref('');
+const qrcodeImage_b64 = ref('');
 
 const generateQRCode = async () => {
   try {
-    console.log('Sending URL:', url.value);
-    
-    const response = await axios.post('/qrcode', { url: url.value });
-    
-    console.log(response);
-
-    qrcodeUrl.value = response.data;
-
+    const response = await axios.post('/qrcode', { url: url.value}, { responseType: "arraybuffer" });
+    qrcodeImage_b64.value = btoa(String.fromCharCode(...new Uint8Array(response.data)));
   } catch (error) {
     console.error('Error generating QR code:', error);
   }
@@ -28,12 +22,12 @@ const generateQRCode = async () => {
     <form id="qrCodeForm" @submit.prevent="generateQRCode">
       <div class="mb-3">
         <label for="url" class="form-label">URL:</label>
-        <input v-model="url" type="text" class="form-control" id="url" required>
+        <input v-model="url" type="text" class="form-control" id="url" placeholder="https://example.com" required>
       </div>
       <button type="submit" class="btn btn-primary">Generate QR Code</button>
     </form>
     <div id="qrcode" class="mt-4">
-      <img :src="qrcodeUrl" alt="Generated QR Code" v-if="qrcodeUrl">
+      <img :src="'data:image/png;base64,' + qrcodeImage_b64" alt="Generated QR Code" v-if="qrcodeImage_b64">
     </div>
   </div>
 </template>
