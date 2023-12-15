9d9237814a7789681375e070eae89cce12825a79
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-15 10:22:59 +0100

    fix(database): fix model for ORM and finish controller function WIP

--- back-end/src/main/controllers.py
+++ back-end/src/main/controllers.py
@@ -1,14 +1,18 @@
 from flask import current_app
 
-from src.main.models import db, Message
+from models import Message
+from database import db
 
 
 def guest_book_GET(limit=10):
     with current_app.app_context():
-        stmt = db.select(Message).order_by(Message.date.desc())
+        # make statement
+        stmt = db.select(Message).order_by(Message.date.desc()).limit(limit)
 
-        messages = db.execute(stmt)
+        # execute query
+        messages = db.session.execute(stmt).scalars()
 
+        # return messages as list of dict (JSON)
         return [message.to_json() for message in messages]
 
 def guest_book_POST(session):
--- back-end/src/main/models.py
+++ back-end/src/main/models.py
@@ -1,24 +1,17 @@
 from sqlalchemy import Integer, DateTime, String
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column
 
-
-# ORM base class
-class Base(DeclarativeBase):
-    pass
-
-
-db = SQLAlchemy(model_class=Base)
+from database import db
 
 
 class Message(db.Model):
-    id_msg: Mapped[Integer] = mapped_column(Integer, primary_key=True)
+    id_message: Mapped[Integer] = mapped_column(Integer, primary_key=True)
     date: Mapped[DateTime] = mapped_column(DateTime)
     text: Mapped[String] = mapped_column(String(500))
 
     def to_json(self):
         return {
-            "id": self.id_msg,
+            "id": self.id_message,
             "date": str(self.date),
             "text": self.text
         }
