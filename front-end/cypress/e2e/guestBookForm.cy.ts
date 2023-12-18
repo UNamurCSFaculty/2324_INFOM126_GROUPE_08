0b34294d1053acf4a832a43db513a0fd6eaae962
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-18 14:14:07 +0100

    refactor(front-end): fix indents

--- front-end/cypress/e2e/guestBookForm.cy.ts
+++ front-end/cypress/e2e/guestBookForm.cy.ts
@@ -1,19 +1,19 @@
 /// <reference types="cypress" />
 
 describe('GuestBook Form', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-  
-    it('submits the form with valid data', () => {
-      cy.get('#nom').type('Maxou');
-      cy.get('#message').type('This is a test message.');
-  
-      cy.get('#formGuestBook').submit();
-  
-      cy.wait(1000); 
-  
-      cy.get('.success-message').should('contain.text', 'Entry successfully added');
-    });
+  beforeEach(() => {
+    cy.visit('/');
   });
+  
+  it('submits the form with valid data', () => {
+    cy.get('#nom').type('Maxou');
+    cy.get('#message').type('This is a test message.');
+ 
+    cy.get('#formGuestBook').submit();
+ 
+    cy.wait(1000); 
+ 
+    cy.get('.success-message').should('contain.text', 'Entry successfully added');
+  });
+});
   
--- front-end/cypress/e2e/qrcodeGenerator.cy.ts
+++ front-end/cypress/e2e/qrcodeGenerator.cy.ts
@@ -2,15 +2,13 @@
 /// <reference types="cypress" />
 
 describe('QR Code Generator Tests', () => {
-    it('should generate a QR code', () => {
-      cy.visit('/');
+  it('should generate a QR code', () => {
+    cy.visit('/');
   
-
-      cy.get('#url').type('https://example.com');
-      cy.get('form').submit();
-      cy.get('#qrcode img').should('be.visible');
-      cy.get('#qrcode img').should('have.attr', 'src').and('include', 'data:image/png;base64,');
-    });
-
+    cy.get('#url').type('https://example.com');
+    cy.get('form').submit();
+    cy.get('#qrcode img').should('be.visible');
+    cy.get('#qrcode img').should('have.attr', 'src').and('include', 'data:image/png;base64,');
   });
+});
   
