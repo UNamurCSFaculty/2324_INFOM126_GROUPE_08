adbf9f2af545ee500b4ee162d2d6b627a11788b7
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-20 17:14:09 +0100

    refactor(tests): rearrange base test file

--- back-end/tests/test_base.py
+++ back-end/tests/test_base.py
@@ -1,5 +1,10 @@
 def test_server_running(client):
+    # arrange
+    ROOT_TEXT = b"<h1>Not Found</h1>"
+
+    # act
     response = client.get("/")
 
-    assert b"<h1>Not Found</h1>" in response.data
+    # assert
+    assert ROOT_TEXT in response.data
     assert response.status_code == 404
