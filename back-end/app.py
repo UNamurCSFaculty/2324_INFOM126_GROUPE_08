2b19a18f4bc7f113b54681bb7114aa018ef744a3
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-20 12:47:20 +0100

    refactor: change files content due to back-end project reorganization

--- back-end/app.py
+++ back-end/app.py
@@ -1,36 +1,6 @@
-from flask import Flask
-from flask_cors import CORS
+from api import create_app
+from config import Database, API
 
-from endpoints import guest_book_blueprint, qrcode_blueprint
-from database import db
-from configs import Database, API
-
-
-def create_app(database_URI, debug=False, **kwargs):
-    """ Application factory to create the app object. """
-
-    # create Flask app
-    app = Flask(__name__)
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_URI
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = debug
-
-    # add CORS header to back request
-    CORS(app)
-
-    # initialize the app with the extension
-    db.init_app(app)
-
-    # create tables if not created yet
-    with app.app_context():
-        db.create_all()
-
-    # register blueprints
-    app.register_blueprint(guest_book_blueprint)
-    app.register_blueprint(qrcode_blueprint)
-
-    # return app object
-    return app
 
 if __name__ == '__main__':
     app = create_app(Database.URI())
