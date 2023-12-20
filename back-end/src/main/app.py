55f9fd87f5fcf3b3999bc3683106530f666f91a4
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-20 12:07:29 +0100

    feat(back-end): create an app factory that will be used for tests

--- back-end/src/main/app.py
+++ back-end/src/main/app.py
@@ -5,25 +5,33 @@ from endpoints import guest_book_blueprint, qrcode_blueprint
 from database import db
 from configs import Database, API
 
-# create Flask app
-app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = Database.URI()
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+def create_app(database_URI, debug=False, **kwargs):
+    """ Application factory to create the app object. """
 
-# add CORS header to back request
-CORS(app)
+    # create Flask app
+    app = Flask(__name__)
 
-# initialize the app with the extension
-db.init_app(app)
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = debug
 
-# create tables if not created yet
-with app.app_context():
-    db.create_all()
+    # add CORS header to back request
+    CORS(app)
 
-# register blueprints
-app.register_blueprint(guest_book_blueprint)
-app.register_blueprint(qrcode_blueprint)
+    # initialize the app with the extension
+    db.init_app(app)
+
+    # create tables if not created yet
+    with app.app_context():
+        db.create_all()
+
+    # register blueprints
+    app.register_blueprint(guest_book_blueprint)
+    app.register_blueprint(qrcode_blueprint)
+
+    # return app object
+    return app
 
 if __name__ == '__main__':
+    app = create_app(Database.URI())
     app.run(host=API.HOST, port=API.PORT)
