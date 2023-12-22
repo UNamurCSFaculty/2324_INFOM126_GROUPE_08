50ca10d4c8b18392aadb0a6e14073db173740b05
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-12-22 19:13:57 +0100

    feat(front-end): set config from environment variables (dev only)

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -5,6 +5,6 @@ import "bootstrap"
 import App from './App.vue'
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = `${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}`;
 
 createApp(App).mount('#app')
